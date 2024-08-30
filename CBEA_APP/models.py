b670c1bf0ead8f236554141d99b6504f93798936
Gabriel Kowalczyk <gabriel.gruckowski@gmail.com>
2024-08-29 21:45:40 -0300

    Cadastro Animais #3

--- CBEA_APP/models.py
+++ CBEA_APP/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Animal(models.Model):
+
+    nome_animal = models.CharField(max_length=100)
+    especie = models.CharField(max_length=100)
+    raca = models.CharField(max_length=100)
+    peso = models.FloatField()
+    idade = models.FloatField()
+    rfid = models.CharField(max_length=20)
