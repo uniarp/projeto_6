53a6aab3cafcb4ba36603a7e55fa81dbd4058515
unknown <monteirofp1985@gmail.com>
2024-09-07 12:23:25 -0300

    branch classe tutor em models.py

--- CBEA_APP/models.py
+++ CBEA_APP/models.py
@@ -17,3 +17,12 @@ class Funcionario(models.Model):
     loginFuncionario = models.CharField(max_length=100),
     senhaFuncionario = models.CharField(max_length=100)
 
+class Tutor(models.Model):
+    codigotutor = models.AutoField(primary_key=True)
+    nometutor = models.CharField(max_length=100, unique=True)
+    cpf = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    rg = models.CharField(max_length=100)
+    cadunico = models.CharField(max_length=100)
